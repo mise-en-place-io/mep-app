8e943f450d22715b6d9e6a7f573b43aa704ab94a
michallapkowski <48141489+michallapkowski@users.noreply.github.com>
2025-04-24 21:50:08 -0700

    deployment: auth

--- auth/config.ts
+++ auth/config.ts
@@ -1,12 +1,10 @@
 export const firebaseConfig = {
   // Your web app's Firebase configuration here
   // See https://firebase.google.com/docs/web/setup#add-sdks-initialize
-  apiKey: 'API_KEY',
-  authDomain: 'PROJECT_ID.firebaseapp.com',
-  databaseURL: 'https://PROJECT_ID.firebaseio.com',
-  projectId: 'PROJECT_ID',
-  storageBucket: 'PROJECT_ID.appspot.com',
-  messagingSenderId: 'SENDER_ID',
-  appId: 'APP_ID',
-  measurementId: 'G-MEASUREMENT_ID',
+  apiKey: "AIzaSyDyjr--cZe5tXX5CIjNX_YD7pUCqY9SSEI",
+  authDomain: "affable-skill-457522-b6.firebaseapp.com",
+  projectId: "affable-skill-457522-b6",
+  storageBucket: "affable-skill-457522-b6.firebasestorage.app",
+  messagingSenderId: "458231150088",
+  appId: "1:458231150088:web:4d67b369faddb2ef0b03bd"
 };
